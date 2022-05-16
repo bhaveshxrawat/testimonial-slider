af509dca832f8d8f87b8760e32a7073b5771f386
Bhavesh Rawat <rawatbhavesh2001@gmail.com>
2022-05-17 02:25:41 +0530

    last single branch where flex breaks the slick slider

---
+++ mock.js
@@ -0,0 +1,52 @@
+// $(".slideit").slick({
+//         slidesToShow: 1,
+//         // slidesToScroll: 1,
+//         arrows: false,
+//         dots: false,
+//         infinite: true,
+//         focusOnSelect: true,
+//         cssEase: "linear",
+//         touchMove: false,
+//         responsive: [
+//           {
+//             breakpoint: 1200,
+//             settings: {
+//               slidesToShow: 2,
+//               slidesToScroll: 1,
+//             },
+//           },
+//           {
+//             breakpoint: 991,
+//             settings: {
+//               slidesToShow: 2,
+//               slidesToScroll: 1,
+//             },
+//           },
+
+//           {
+//             breakpoint: 768,
+//             settings: {
+//               variableWidth: true,
+//               slidesToShow: 2,
+//               slidesToScroll: 1,
+//             },
+//           },
+//         ],
+//       });
+//     //   $(".domain").slick({
+//     //     dots: true,
+//     //     infinite: true,
+//     //     speed: 300,
+//     //     slidesToShow: 4,
+//     //     slidesToScroll: 1,
+//     //     centerMode: true,
+//     //     variableWidth: true,
+//     //   });
+//       $("#prev_tes").on("click", function (e) {
+//         e.preventDefault();
+//         $(".slideit").slick("slickPrev");
+//       });
+//       $("#next_tes").on("click", function (e) {
+//         e.preventDefault();
+//         $(".slideit").slick("slickNext");
+//       });
